173679eedffc79bebb7764a433cd1cb40e69e9fe
Sam McCord <samuel.e.mccord@gmail.com>
2015-01-14 10:20:32 -0500

    starting product directive and branching out.

--- client/app/store-temp/store.controller.js
+++ client/app/store-temp/store.controller.js
@@ -1,22 +1,24 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .controller('StoreCtrl', function ($scope,$http, socket,$stateParams,$resource,Auth,User,Store) {
-  	$scope.owner = false;
-    $scope.store = {};
+    .controller('StoreCtrl', function($scope, $http, socket, $stateParams, $resource, Auth, User, Store) {
+        $scope.owner = false;
+        $scope.store = {};
 
-    $scope.ownerPresent = false;
+        $scope.ownerPresent = false;
 
-    var store = Store.get({name:$stateParams.name},function(store){
-    	console.log(store);
-    	  	User.get().$promise
-			  	.then(function(user){
-			  		console.log(user._id)
-			  		console.log(store.owner)
-			  		if(user._id == store.owner){
-			  			$scope.owner = true;
-			  		}
-			  	})
-    })
+        var store = Store.get({
+            name: $stateParams.name
+        }, function(store) {
+            $scope.store = store;
+            User.get().$promise
+                .then(function(user) {
+                    console.log(user._id)
+                    console.log(store.owner)
+                    if (user._id == store.owner) {
+                        $scope.owner = true;
+                    }
+                })
+        })
 
-  });
+    });
