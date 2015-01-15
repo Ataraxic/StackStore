cab3089936e8cfb6ca7d781b180601a49498526b
Sam McCord <samuel.e.mccord@gmail.com>
2015-01-15 18:04:49 -0500

    cart route and nav dropdown

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .controller('CartCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+    .controller('CartCtrl', function($scope, $resource, Auth, User, Store, Cart, Product, $location, $http) {
+        $scope.message = 'Hello';
+
+        Cart.get(function(err, data) {
+        	$scope.cart_page = data.cart;
+        });
+    });
