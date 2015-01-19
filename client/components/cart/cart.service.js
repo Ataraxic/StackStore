06929a620e4d887d9575f85283ac477aed84c4bf
Sam McCord <samuel.e.mccord@gmail.com>
2015-01-19 16:36:29 -0500

    checked for length to fix reduce issue

--- client/components/cart/cart.service.js
+++ client/components/cart/cart.service.js
@@ -3,47 +3,47 @@
 angular.module('stackStoreApp')
     .factory('Cart', function(socket, User, $http, Auth, localStorageService) {
 
-            var user;
-            var cart = {
-                ids: []
-            };
+        var user;
+        var cart = {
+            ids: []
+        };
 
+        if (Auth.isLoggedIn()) {
+            user = Auth.getCurrentUser();
+        } else {
+            cart.ids = localStorageService.get('cart') ? localStorageService.get('cart') : [];
+        }
+
+        function update(cart, callback) {
             if (Auth.isLoggedIn()) {
                 user = Auth.getCurrentUser();
-            } else {
-                cart.ids = localStorageService.get('cart') ? localStorageService.get('cart') : [];
-            }
-
-            function update(cart, callback) {
-                if (Auth.isLoggedIn()) {
-                    user = Auth.getCurrentUser();
-                    $http.put('/api/users/' + user._id + '/cart', {
-                            action: 'update',
-                            cart: cart
-                        })
-                        .success(function(user) {
-                            get(function(err, user) {
-                                callback(err, user);
-                            });
-                        })
-                        .error(function(err) {
-                            console.log(err);
-                            callback(err);
-                        });
-                }
-            }
-
-            function mergeCarts(cart, callback) {
-                var newCart = [];
-                if (localStorageService.get('cart').length > 0) {
-                    cart.ids = localStorageService.get('cart');
-                    cart.forEach(function(id) {
-                        if (cart.ids.lastIndexOf(id) > -1) {
-                            cart.ids.splice(cart.ids.lastIndexOf(id), 1);
-                        }
+                $http.put('/api/users/' + user._id + '/cart', {
+                        action: 'update',
+                        cart: cart
                     })
-                    newCart = cart.concat(cart.ids);
-                    localStorageService.set('cart', []);
+                    .success(function(user) {
+                        get(function(err, user) {
+                            callback(err, user);
+                        });
+                    })
+                    .error(function(err) {
+                        console.log(err);
+                        callback(err);
+                    });
+            }
+        }
+
+        function mergeCarts(cart, callback) {
+            var newCart = [];
+            if (localStorageService.get('cart').length > 0) {
+                cart.ids = localStorageService.get('cart');
+                cart.forEach(function(id) {
+                    if (cart.ids.lastIndexOf(id) > -1) {
+                        cart.ids.splice(cart.ids.lastIndexOf(id), 1);
+                    }
+                })
+                newCart = cart.concat(cart.ids);
+                localStorageService.set('cart', []);
                 update(newCart, function(err, user) {
                     if (err) console.log(err)
                     callback()
@@ -69,20 +69,19 @@ angular.module('stackStoreApp')
         }
 
         function formatCartObj(cart) {
-        	var formatted;
-        	if(user){
-        		formatted = cart.reduce(function(prev,curr,index,array){
-        			if(!prev[curr._id]){
-        				prev[curr._id] = curr;
-        				prev[curr._id].quantity = 1;
-        			}
-        			else{
-        				prev[curr._id].quantity += 1;
-        			}
-        			return prev;
-        		},{});
-        	}
-        	return formatted;
+            var formatted;
+            if (user) {
+                formatted = cart.reduce(function(prev, curr, index, array) {
+                    if (!prev[curr._id]) {
+                        prev[curr._id] = curr;
+                        prev[curr._id].quantity = 1;
+                    } else {
+                        prev[curr._id].quantity += 1;
+                    }
+                    return prev;
+                }, {});
+            }
+            return formatted;
         }
 
         function getProductsFromCache(callback) {
@@ -101,44 +100,45 @@ angular.module('stackStoreApp')
         function get(callback) {
             if (Auth.isLoggedIn()) {
                 user = Auth.getCurrentUser();
-                if(localStorageService.get('cart')) {
-                  if (localStorageService.get('cart').length > 0) {
-                      mergeCarts(user.cart, function() {
-                          $http.get('/api/users/' + user._id + '/populate')
-                              .success(function(user) {
-                                  callback(null, user);
-                              })
-                              .error(function(err) {
-                                  console.log(err);
-                                  callback(err);
-                              });
-                      })
-                  } else {
-                      $http.get('/api/users/' + user._id + '/populate')
-                          .success(function(user) {
-                              callback(null, user);
-                          })
-                          .error(function(err) {
-                              console.log(err);
-                              callback(err);
-                          });
-                  }
-              }
+                if (localStorageService.get('cart')) {
+                    if (localStorageService.get('cart').length > 0) {
+                        mergeCarts(user.cart, function() {
+                            $http.get('/api/users/' + user._id + '/populate')
+                                .success(function(user) {
+                                    callback(null, user);
+                                })
+                                .error(function(err) {
+                                    console.log(err);
+                                    callback(err);
+                                });
+                        })
+                    } else {
+                        $http.get('/api/users/' + user._id + '/populate')
+                            .success(function(user) {
+                                callback(null, user);
+                            })
+                            .error(function(err) {
+                                console.log(err);
+                                callback(err);
+                            });
+                    }
+                }
             } else {
                 getProductsFromCache(function(err, products) {
                     if (err) console.log(err);
                     else {
-
-                        var newCart = []
-
-                        var formatted = cart.ids.reduce(function(prev, curr, index, arr) {
-                            products.forEach(function(product) {
-                                if (product._id == curr) {
-                                    newCart.push(product);
-                                }
-                                return prev;
+                        var newCart = [];
+                        var formatted;
+                        if (cart.ids.length > 0) {
+                            formatted = cart.ids.reduce(function(prev, curr, index, arr) {
+                                products.forEach(function(product) {
+                                    if (product._id == curr) {
+                                        newCart.push(product);
+                                    }
+                                    return prev;
+                                })
                             })
-                        })
+                        }
                         console.log(newCart);
                         callback(null, {
                             cart: newCart
