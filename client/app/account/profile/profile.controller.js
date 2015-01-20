3c036351dd09585a59b8a938092cc8837603a4b8
Justin Sung <JustinFSung@gmail.com>
2015-01-20 16:07:04 -0500

    redid user profile page

--- client/app/account/profile/profile.controller.js
+++ client/app/account/profile/profile.controller.js
@@ -1,11 +1,27 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .controller('ProfileCtrl', function ($scope,User,Auth,$stateParams) {
+  .controller('ProfileCtrl', function ($scope,User,Auth,$stateParams,$modal) {
     $scope.name = $stateParams.username;
     User.getUserByName({id: $stateParams.username},function(user){
+      $scope.isProfilePage = true;
+      console.log("this is the user Object",user);
       $scope.favorites = user.favorites;
       $scope.comments = user.comments;
+      $scope.toggleDialog = function(product){
+        var options = {
+          templateUrl: '/app/store-temp/directives/product/modalTemplate.html',
+          windowClass: 'app-modal-window',
+          controller: 'modalController',
+          size: 'lg',
+          resolve: {
+            productId: function (){
+              return product._id;
+            }
+          }
+        };
+        var productModal = $modal.open(options);
+      };
     });
     $scope.addUpvote = function(row){
       row.upvotes+=1;
