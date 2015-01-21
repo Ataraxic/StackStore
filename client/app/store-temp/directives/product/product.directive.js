b9eb26f9e349ca2c75d76dd438b75c9f7823241b
Sam McCord <samuel.e.mccord@gmail.com>
2015-01-20 21:32:31 -0500

    styling

--- client/app/store-temp/directives/product/product.directive.js
+++ client/app/store-temp/directives/product/product.directive.js
@@ -1,28 +1,34 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .directive('product', function (Product) {
-    return {
-      templateUrl: 'app/store-temp/directives/product/product.html',
-      restrict: 'EA',
-      link: function (scope, element, attrs) {
-        // scope: true;
-      },
-      controller: function($scope,$modal){
-        $scope.toggleDialog = function(product){
-          var options = {
-            templateUrl: '/app/store-temp/directives/product/modalTemplate.html',
-            windowClass: 'app-modal-window',
-            controller: 'modalController',
-            size: 'lg',
-            resolve: {
-              productId: function (){
-                return product._id;
-              }
+    .directive('product', function(Product, $stateParams, $location) {
+        return {
+            templateUrl: 'app/store-temp/directives/product/product.html',
+            restrict: 'EA',
+            link: function(scope, element, attrs) {
+                // scope: true;
+            },
+            controller: function($scope, $modal) {
+                $scope.toggleDialog = function(product) {
+                    var options = {
+                        templateUrl: '/app/store-temp/directives/product/modalTemplate.html',
+                        windowClass: 'app-modal-window',
+                        controller: 'modalController',
+                        size: 'lg',
+                        resolve: {
+                            productId: function() {
+                                return product._id;
+                            }
+                        }
+                    };
+
+                    $scope.editRedirect = function(name) {
+                        $location.path('/store/' + $stateParams.name + '/admin/edit/' + name);
+                    }
+
+
+                    var productModal = $modal.open(options);
+                };
             }
-          };
-          var productModal = $modal.open(options);
         };
-      }
-    };
-  });
+    });
